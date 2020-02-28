ec0855a43084db175f5f59836ab6aa41686f11bf
Marcus H Y Leung <mhyleung@cityu.edu.hk>
2020-02-27 16:09:44 -0800

    Delete draw_who_pm.R

--- draw_who_pm.R
+++
@@ -1,7 +0,0 @@
-Table <- read.tidy("worldwide_PAH.txt")
-Table$Country <- factor(Table$Country, levels = c("Baoding, China","Dalian, China","Australia","Canada","China","France","Japan","UK","USA"))
-
-Plot <- ggplot(Table,aes(x=Ten,y=Two,colour=Country))
-Plot <- Plot + geom_point(size=1) + theme_classic() + scale_fill_brewer(palette="Set3")
-Plot <- Plot + xlab(paste0("[PM10] (ug/m3)")) + ylab(paste0("[PM10] (ug/m3)")) + theme(legend.title=element_blank())
-ggsave("WHO_data.pdf",useDingbats=FALSE)
